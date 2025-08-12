afa9b159d36521407c106a8ff94c3426a36ea258
Sannidhanam Hrushikesh Kumar <sannidhanamhrushikesh@gmail.com>
2025-08-13 02:16:41 +0530

    login issue resolved

--- Backend/routes/auth.routes.js
+++ Backend/routes/auth.routes.js
@@ -1,9 +1,13 @@
 import express from 'express';
 import {
-    signUp
+    signUp,
+    login,
+
 } from '../controllers/auth.controller.js';
 
 const router=express.Router();
 router.post('/signUp',signUp);
+router.post('/login',login);
+
 export default router;
 
